bc1f5beaf2eec8a39ee18e79e4a36e3ece50c66b
Dmitry Yevtushenko <yevtushenkods@gmail.com>
2019-08-29 01:08:09 +0300

    added 'clone' function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,15 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let clonedObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      clonedObj[key] = clone(obj[key]);
+    } else {
+      clonedObj[key] = obj[key];
+    }
+  }
+
+  return clonedObj;
 }
